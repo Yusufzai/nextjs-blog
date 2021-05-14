3817369776e77c0c8ac74a6837ced3f37dee6c3b
unknown <owaizyusufi@gmail.com>
2021-05-14 23:45:59 +0530

    initial commit

---
+++ pages/posts/image-post.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+const YourComponent = () => (
+
+    <>
+        <Head>
+            <title>Image Post</title>
+        </Head>
+
+        <Image
+            src="/images/profile.jpg" // Route of the image file
+            height={144} // Desired size with correct aspect ratio
+            width={144} // Desired size with correct aspect ratio
+            alt="Your Name"
+        />
+    </>
+)
+
+export default YourComponent
